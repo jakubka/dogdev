e57c7dad352b74578c0e352430e2251dc612ab4e
Jakub Kadlubiec <jakub.kadlubiec@gmail.com>
2013-10-12 14:47:12 +0200

    Zaklad dog serveru

---
+++ src/DogServer/DogServer/Hubs/PresentationHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace DogServer.Hubs
+{
+    public class State
+    {
+        public bool IsGameStarted { get; set; }
+        
+        public int PlayerOrientation { get; set; }
+        
+        public int PlayerHealth { get; set; }
+
+        public int? CreatureOrientation { get; set; }
+
+        public int? CreatureDistance { get; set; }
+
+        public int FragsCount { get; set; }
+    }
+
+
+    public class PresentationHub : Hub
+    {
+        public void LogState(State state)
+        {
+
+            Clients.Others.displayState(state);
+        }
+    }
+}
