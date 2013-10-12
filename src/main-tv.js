64db1d7c1bd6ee8d0db6904fffb63e68f317661a
mazurroman <roman.mazur@hotmail.com>
2013-10-12 14:23:21 -0400

    tv ui++

--- src/main-tv.js
+++ src/main-tv.js
@@ -5,12 +5,15 @@
         fabric = w.fabric,
         doc = w.document,
         $ = w.$,
+        hub = P.hub,
         canvasWidth = window.innerWidth,
         canvasHeight = window.innerHeight,
+        maxDistanceFromPlayer = (canvasWidth - 20) / 2,
         canvasXMid = canvasWidth / 2,
         canvasYMid = canvasHeight / 2,
         canvas = new fabric.Canvas('c'),
-        boundaryCirc;
+        $compass = $(".compassIco"),
+        boundaryCirc, zombiePoint;
 
     // set canvas to stretch to window
     canvas.setWidth(canvasWidth);
@@ -21,13 +24,36 @@
         left: canvasXMid,
         top: canvasYMid,
         fill: 'transparent',
-        radius: (canvasWidth - 20) / 2,
+        radius: maxDistanceFromPlayer,
         strokeWidth: 1,
         stroke: 'grey'
     });
 
-    // "add" rectangle onto canvas
+    zombiePoint = new fabric.Circle({
+        fill: 'red',
+        radius: 5,
+    });
+
+    // "add" items into canvas
     canvas.add(boundaryCirc);
+    canvas.add(zombiePoint);
+
+    hub.init(function(data) {
+        // update compass
+        $compass.css('-webkit-transform', 'rotate(' + (-data.playerOrientation) + 'deg)');
+
+        var x = (data.creatureDistance / 100 * maxDistanceFromPlayer * Math.cos((data.creatureOrientation - 90) / 180 * Math.PI)) + canvasXMid;
+        var y = (data.creatureDistance / 100 * maxDistanceFromPlayer * Math.sin((data.creatureOrientation - 90) / 180 * Math.PI)) + canvasYMid;
+
+        // update zombie
+        zombiePoint.set({
+            left: x,
+            top: y
+        });
+
+        // update canvas
+        canvas.renderAll();
+    });
 
     // setTimeout(function() {
     //     boundaryCirc.set({
