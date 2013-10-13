13f86b8b6f618cc72f2a7c6bf79369bc2fc54a80
Jakub Kadlubiec <jakub.kadlubiec@gmail.com>
2013-10-13 08:32:54 +0200

    gfdgd

--- DogServer/static/js/main-tv.js
+++ DogServer/static/js/main-tv.js
@@ -12,7 +12,7 @@
         canvasXMid = canvasWidth / 2,
         canvasYMid = canvasHeight / 2,
         canvas = new fabric.Canvas('c'),
-        boundaryCirc, zombiePoint, movingLine, zombieVisible, crossLine1, crossLine2, innerCirc1, innerCirc2,triangle, text1, text2, text3;
+        boundaryCirc, zombiePoint, movingLine, zombieVisible, crossLine1, crossLine2, innerCirc1, innerCirc2, triangle, text1, text2, text3;
 
     // set canvas to stretch to window
     canvas.setWidth(canvasWidth);
@@ -25,9 +25,10 @@
         fill: 'transparent',
         radius: maxDistanceFromPlayer,
         strokeWidth: 2,
-        stroke: 'green'
+        stroke: 'green',
+        selectable: false
     });
-    
+
     innerCirc1 = new fabric.Circle({
         left: canvasXMid,
         top: canvasYMid,
@@ -35,7 +36,8 @@
         radius: maxDistanceFromPlayer * 0.4,
         strokeWidth: 1,
         stroke: 'green',
-        strokeDashArray: [10, 5]
+        strokeDashArray: [10, 5],
+        selectable: false
     });
     innerCirc2 = new fabric.Circle({
         left: canvasXMid,
@@ -45,17 +47,19 @@
         strokeWidth: 1,
         stroke: 'green',
         strokeDashArray: [10, 5],
+        selectable: false
     });
 
-    
+
     triangle = new fabric.Triangle({
-        width: 20, 
-        height: 30,
-        fill: 'red', 
-        left: canvasXMid, 
-        top: canvasYMid, 
+        width: 40,
+        height: 60,
+        fill: 'red',
+        left: canvasXMid,
+        top: canvasYMid,
         angle: 0,
         opacity: 0.5,
+        selectable: false
         // originX: 1000,
         // originY: 100,
     });
@@ -71,23 +75,55 @@
     
 
 
-    text1 = new fabric.Text('frags: 000', { backgroundColor: 'rgb(0,200,0)', left: 100, top: 100,  textAlign: 'left' });
-    text2 = new fabric.Text('health: 000', { backgroundColor: 'rgb(0,200,0)', left: 100, top: 150, textAlign: 'left' });
-    text3 = new fabric.Text('DISCONNECTED', { opacity: 1, backgroundColor: 'rgb(0,200,0)', left: 600, top: 150,  fontSize: 150, height: 2000 });
+    text1 = new fabric.Text('frags: 000', {
+        backgroundColor: 'rgb(0,200,0)',
+        left: 100,
+        top: 100,
+        textAlign: 'left',
+        selectable: false
+    });
+    text2 = new fabric.Text('health: 000', {
+        backgroundColor: 'rgb(0,200,0)',
+        left: 100,
+        top: 150,
+        textAlign: 'left',
+        selectable: false
+    });
+    text3 = new fabric.Text('DISCONNECTED', {
+        opacity: 1,
+        backgroundColor: 'rgb(0,200,0)',
+        left: 600,
+        top: 150,
+        fontSize: 150,
+        height: 2000,
+        selectable: false
+    });
 
 
-    crossLine1 = new fabric.Line([canvasXMid, canvasYMid - maxDistanceFromPlayer - 10, canvasXMid, canvasYMid + maxDistanceFromPlayer + 10], {stroke: 'green',strokeWidth: 1,strokeDashArray: [10, 5]});
-    crossLine2 = new fabric.Line([canvasXMid - maxDistanceFromPlayer - 10, canvasYMid, canvasXMid + maxDistanceFromPlayer + 10, canvasYMid], {stroke: 'green',strokeWidth: 1,strokeDashArray: [10, 5]});
+    crossLine1 = new fabric.Line([canvasXMid, canvasYMid - maxDistanceFromPlayer - 10, canvasXMid, canvasYMid + maxDistanceFromPlayer + 10], {
+        stroke: 'green',
+        strokeWidth: 1,
+        strokeDashArray: [10, 5],
+        selectable: false
+    });
+    crossLine2 = new fabric.Line([canvasXMid - maxDistanceFromPlayer - 10, canvasYMid, canvasXMid + maxDistanceFromPlayer + 10, canvasYMid], {
+        stroke: 'green',
+        strokeWidth: 1,
+        strokeDashArray: [10, 5],
+        selectable: false
+    });
 
     movingLine = new fabric.Line([canvasXMid, canvasYMid, canvasXMid + maxDistanceFromPlayer, canvasYMid], {
         stroke: 'green',
-        strokeWidth: 3
+        strokeWidth: 3,
+        selectable: false
     });
 
     zombiePoint = new fabric.Circle({
         fill: 'red',
         radius: 10,
         opacity: 0,
+        selectable: false
     });
 
     zombieVisible = false;
@@ -104,9 +140,6 @@
     canvas.add(text1);
     canvas.add(text2);
     canvas.add(text3);
-    
-
-    
 
 
     (function animate() {
@@ -116,7 +149,7 @@
             startValue: 0,
             endValue: 360,
             duration: 2500,
-            easing: function(t, b, c, d) { return c*t/d + b; },
+            easing: function(t, b, c, d) { return c * t / d + b; },
             onChange: function(angle) {
                 angle = fabric.util.degreesToRadians(angle);
                 var x = canvasXMid + maxDistanceFromPlayer * Math.cos(angle);
@@ -127,57 +160,57 @@
                     y2: y
                 }).setCoords();
                 zombiePoint.setCoords();
-                if(!zombieVisible && zombiePoint.intersectsWithObject(movingLine)) {
-                        zombieVisible = true;
+                if (!zombieVisible && zombiePoint.intersectsWithObject(movingLine)) {
+                    zombieVisible = true;
                     // Animate point
                     fabric.util.animate(
-                    {
-                        startValue: -0.7,
-                        endValue: 0.7,
-                        duration: 2500,
-                        onChange: function(opacity) {
-                            zombiePoint.opacity = 1-Math.abs(opacity);
-                            canvas.renderAll();
-                        },
-                        onComplete: function() {
-                            zombieVisible = false;
+                        {
+                            startValue: 0,
+                            endValue: 0.7,
+                            duration: 2500,
+                            onChange: function(opacity) {
+                                zombiePoint.opacity = 1 - Math.abs(opacity);
+                                canvas.renderAll();
+                            },
+                            onComplete: function() {
+                                zombieVisible = false;
+                            }
                         }
-                    }
                     );
                 }
 
                 canvas.renderAll();
             },
             onComplete: animate
-
         });
     })();
 
 
     hub.init(function(data) {
-        // update compass
-        // $compass.css('-webkit-transform', 'rotate(' + (-data.playerOrientation) + 'deg)');
-
         triangle.angle = data.PlayerOrientation;
         text1.setText("frags: " + data.FragsCount);
         text2.setText("health: " + data.PlayerHealth);
 
-        if (data.IsGameStarted) {
-            text3.opacity = 0;
-        } else {
-            text3.opacity = 1;
+        if (data.IsGameStarted && canvas.contains(text3)) {
+            canvas.remove(text3);
+        } else if (!data.IsGameStarted && !canvas.contains(text3)) {
+            canvas.add(text3);
         }
 
+        if (data.CreatureDistance == null) {
+            zombiePoint.setVisible = false;
+        } else {
+            var x = (data.CreatureDistance / 100 * maxDistanceFromPlayer * Math.cos((data.CreatureOrientation - 90) / 180 * Math.PI)) + canvasXMid;
+            var y = (data.CreatureDistance / 100 * maxDistanceFromPlayer * Math.sin((data.CreatureOrientation - 90) / 180 * Math.PI)) + canvasYMid;
 
-        var x = (data.CreatureDistance / 100 * maxDistanceFromPlayer * Math.cos((data.CreatureOrientation - 90) / 180 * Math.PI)) + canvasXMid;
-        var y = (data.CreatureDistance / 100 * maxDistanceFromPlayer * Math.sin((data.CreatureOrientation - 90) / 180 * Math.PI)) + canvasYMid;
-
-        // update zombie
-        zombiePoint.set({
-            left: x,
-            top: y,
-            opacity: 0,
-        });
+            // update zombie
+            zombiePoint.set({
+                left: x,
+                top: y,
+                visible: true,
+                opacity: 0
+            });
+        }
 
         // update canvas
         canvas.renderAll();
